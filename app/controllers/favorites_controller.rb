0f1b1e5fef685fbbc787285bc849668be37a0d52
takashi oka <takashi.oka@gmail.com>
2021-04-18 22:26:38 +0900

    css,js,bootstrap fixed

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,14 @@
 class FavoritesController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @favourites = current_user.favourites.all
+    @favorites = current_user.favorites.all
   end
   def create
-    favourite = current_user.favourites.create(picture_id: params[:picture_id])
-    redirect_to pictures_url, notice: "#{favourite.picture.user.name}さんの投稿をお気に入り登録しました"
+    favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの投稿をお気に入り登録しました"
   end
   def destroy
-    favourite = current_user.favourites.find_by(id: params[:id]).destroy
-    redirect_to pictures_url, notice: "#{favourite.picture.user.name}さんの投稿をお気に入り解除しました"
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの投稿をお気に入り解除しました"
   end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,5 +3,6 @@ class Picture < ApplicationRecord
   has_many :favourites, dependent: :destroy
   has_many :favourite_users, through: :favourites, source: :user
   validates :content, presence: true
+  validates :image, presence: true
   mount_uploader :image, ImageUploader
 end
